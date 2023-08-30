9ec78d956b88bde66d9d985b65d0fc32f074cc36
rifandani <ipandani2505@gmail.com>
2023-08-30 21:42:06 +0700

    feat: initial impl

---
+++ observer-async.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * fake async operations
+ *
+ * @param {number} ms
+ */
+function sleep(ms) {
+  console.info(`Sleeping for ${ms}ms`);
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+class ObservableAsync {
+  constructor() {
+    this.observers = [];
+  }
+
+  /**
+   * Subscribe to changes in the data
+   *
+   * @param {() => void} callback
+   */
+  subscribe(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('Callback must be a function');
+    }
+    this.observers.push(callback);
+  }
+
+  /**
+   *
+   * @param {() => void} callback
+   */
+  unsubscribe(callback) {
+    [...this.observers].forEach((observer, index) => {
+      if (observer === callback) {
+        this.observers.splice(index, 1);
+      }
+    });
+  }
+
+  /**
+   * notify and wait for all updates to complete
+   *
+   * @param {any} value
+   */
+  async notify(value) {
+    // Call the subscribed function and wait for it to resolve
+    const updates = this.observers.map(async (callback) => {
+      await callback(value);
+    });
+
+    await Promise.allSettled(updates);
+  }
+}
