2545aa920a13fe48dc4a7a899cea26b601d8a5a6
rifandani <ipandani2505@gmail.com>
2023-09-10 22:38:31 +0700

    feat: initial implementation

---
+++ webcomponent.js
@@ -0,0 +1,74 @@
+class ThemeText extends HTMLElement {
+  static get observedAttributes() {
+    return ['data-theme'];
+  }
+
+  constructor() {
+    super();
+    // If you attach a shadow root to a custom element with mode: "closed" set, you won't be able to access the shadow DOM from the outside
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+
+    const p = document.createElement('p');
+    p.textContent = 'Selected Theme: ';
+
+    const span = document.createElement('span');
+    span.textContent = this.getAttribute('data-theme');
+
+    const style = document.createElement('style');
+    style.textContent = `
+    .bg-white {
+      background-color: azure;
+    }
+    .bg-black {
+      background-color: slategrey;
+    }
+    .text-white {
+      color: beige;
+    }
+    .text-black {
+      color: brown;
+    }
+    `;
+
+    p.appendChild(span);
+    shadowRoot.appendChild(p);
+    shadowRoot.appendChild(style);
+  }
+
+  /**
+   * Invoked each time the custom element is appended into a document-connected element.
+   * This will happen each time the node is moved, and may happen before the element's contents have been fully parsed.
+   */
+  connectedCallback() {
+    console.log(`connectedCallback invoked`);
+  }
+
+  /**
+   * Invoked each time the custom element is disconnected from the document's DOM.
+   */
+  disconnectedCallback() {
+    console.log(`disconnectedCallback invoked`);
+  }
+
+  /**
+   * Invoked each time the custom element is moved to a new document.
+   */
+  adoptedCallback() {
+    console.log(`adoptedCallback invoked`);
+  }
+
+  /**
+   * Invoked each time one of the custom element's attributes is added, removed, or changed
+   * Which attributes to notice change for is specified in a `static get observedAttributes` method
+   */
+  attributeChangedCallback(name, _oldValue, newValue) {
+    if (name === 'data-theme') {
+      const p = this.shadowRoot.querySelector('p');
+      const span = this.shadowRoot.querySelector('span');
+
+      p.className =
+        newValue === 'light' ? 'bg-white text-black' : 'bg-black text-white';
+      span.textContent = newValue || '';
+    }
+  }
+}
