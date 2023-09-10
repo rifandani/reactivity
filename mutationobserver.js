d0a9c127dad2c234f36c9f91415b7b6b04b538c8
rifandani <ipandani2505@gmail.com>
2023-09-10 21:32:46 +0700

    feat: initial implementation

---
+++ mutationobserver.js
@@ -0,0 +1,19 @@
+/** @type {MutationCallback} */
+const mutationCallback = (mutations) => {
+  for (const mutation of mutations) {
+    if (
+      mutation.type !== 'attributes' ||
+      mutation.attributeName !== 'data-theme'
+    )
+      return;
+
+    /** @type {HTMLElement} */
+    const body = mutation.target;
+    const newTheme = body.getAttribute('data-theme'); // new value
+
+    body.className =
+      newTheme === 'light' ? 'bg-white text-black' : 'bg-black text-white';
+  }
+};
+
+const observer = new MutationObserver(mutationCallback);
