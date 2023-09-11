161f9e0afd7d346433a6a845b851323167cf8173
rifandani <ipandani2505@gmail.com>
2023-09-11 22:52:55 +0700

    feat: add typing

--- observer.js
+++ observer.js
@@ -1,10 +1,27 @@
+/** @typedef {(data: any) => void} Observer */
+
 // Subscribers, which can subscribe to and get notified by the observer object.
+/** @type {Array<Observer>} */
 const observers = [];
 
 // An observer object, which can be observed by subscribers in order to notify them.
 const observer = Object.freeze({
+  /**
+   * a method to notify all observers whenever a specific event occurs
+   *
+   * @param {any} data
+   */
   notify: (data) => observers.forEach((observer) => observer(data)),
+  /**
+   * a method in order to add observers to the observers list
+   *
+   * @param {Observer} func
+   */
   subscribe: (func) => observers.push(func),
+  /**
+   *
+   * @param {Observer} func
+   */
   unsubscribe: (func) => {
     [...observers].forEach((observer, index) => {
       if (observer === func) {
